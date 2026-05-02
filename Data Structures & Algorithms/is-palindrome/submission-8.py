398a1c7557a8048e0998f757605cd4d311a6a00e
HalgasAdrian <71724269+HalgasAdrian@users.noreply.github.com>
2026-05-02 16:13:44 -0400

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        testString = s.lower()
+        sList = list(testString)
+        front = 0
+        back = len(s) -  1
+
+        if len(s) == 0:
+            return True
+
+        while front < back:
+            while front < back and not sList[front].isalnum():
+                front += 1
+            while back > front and not sList[back].isalnum():
+                back -= 1
+            if sList[front] == sList[back]:
+                front += 1
+                back -= 1
+            else:
+                return False 
+        
+        return True
+
+        
